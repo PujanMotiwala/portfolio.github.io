1c38ff8c5eb904cc756a38967946c41c23f64247
Pujan Motiwala <motiwalapujan@gmail.com>
2021-02-17 16:50:03 -0600

    HomePage Integrations

--- mywebapp/src/App.js
+++ mywebapp/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import Navs from './components/Navs';
+import Box1 from './components/Box1';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component{
+    state={pageHeader : "Username"/*,
+			data: [12, 5, 6, 6, 9, 10],
+    		width: 700,
+    		height: 500,
+    		id: root*/};
+
+    componentDidMount(){
+        //AJAX...(fetching)
+        //fire timers and/or listeners (..to other events)
+    }
+
+    componentWillUnmount(){
+        //clean timers and/or listeners (..above ones)
+    }
+    render(){
+        return (
+        <div className="App">
+            <header className="App-header">
+                <Col>
+                    <Col >
+                        <Row>
+                            <Navs/>
+                        </Row>
+                    </Col>
+                    <br/>
+                        <Col sm={{ span: 4, offset: 1 }}>
+                            <h2>Data Scientist | Big Data Engineer | Visualizer</h2>
+                        </Col>
+                        <Col md={{ span: 3, offset: 0 }}>
+
+
+
+                            <h1>Welcome :) This is my internet home!</h1>
+
+
+                        </Col>
+
+
+
+                    <br/>
+                    <br/>
+
+                    <Row sm={10}>
+                        <Col sm={2}>
+
+                        </Col>
+                    <Col sm={7}>
+                        <Box1/>
+                    </Col>
+                    </Row>
+
+
+                </Col>
+
+
+
+
+            </header>
+        </div>
   );
+}
 }
 
 export default App;
--- mywebapp/src/index.js
+++ mywebapp/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// <React.StrictMode>
+// </React.StrictMode>,
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <App />,
+
   document.getElementById('root')
 );
 
